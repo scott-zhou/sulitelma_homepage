15e10d78bdb385286bb80bb7cdc6a8b884ee60e5
Scott Zhou <scott.cong.zhou@ericsson.com>
2015-11-11 17:22:41 +0100

    Remove rails files. Move to static pages.

--- app/helpers/static_pages_helper.rb
+++
@@ -1,9 +0,0 @@
-module StaticPagesHelper
-  def my_name
-    'Shirmen Chen'
-  end
-
-  def my_email
-    'shirmen.chen@sulitelma.se'
-  end
-end
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root 'static_pages#home'
-  get  'about'   =>  'static_pages#about'
-  get  'contact' =>  'static_pages#contact'
-end
